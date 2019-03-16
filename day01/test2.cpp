8d5f7c6e65f2521adf73a878e8469ded0e80d039
wanlong <wanlong3@jd.com>
2019-03-16 17:15:12 +0800

    第一天的一些理解及处理

---
+++ day01/test2.cpp
@@ -0,0 +1,13 @@
+//
+// Created by wanlong3 on 2019/3/16.
+//
+#include<iostream>
+using namespace std;
+int DemoConsoleOutput(){
+    cout<<"等于多少"<<20.1/3<<endl;
+    return 0;
+}
+int main(){
+    DemoConsoleOutput();
+    
+}
