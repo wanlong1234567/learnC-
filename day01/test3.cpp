b5bc45240b0dc284eaf435b39c7a001ff17685f2
wanlong <wanlong3@jd.com>
2019-03-16 19:41:28 +0800

    第一天的一些理解及处理2

---
+++ day01/test3.cpp
@@ -0,0 +1,14 @@
+//
+// Created by wanlong3 on 2019/3/16.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    int InputNumber;
+    cout<<"Enter an integer:"<<endl;
+    cin>>InputNumber;
+    cout<<"Enter your name:"<<endl;
+    string inputName;
+    cin>>inputName;
+    cout<<inputName<<","<<InputNumber<<endl;
+}
